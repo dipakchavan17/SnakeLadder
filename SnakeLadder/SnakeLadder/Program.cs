8446fbb022b8fe3c18229971a06fd71e3598b2bf
Dipak@1432 <chavandipakr17@gmail.com>
2022-01-25 21:28:48 +0530

    welcome to SnakeLadder

---
+++ SnakeLadder/SnakeLadder/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SnakeLadder
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("welcome to snake ladder");
+            Console.ReadLine();
+        }
+    }
+}
