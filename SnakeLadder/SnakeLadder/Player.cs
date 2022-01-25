8c3c1d1fa86932fc6bef1ebdaa40dd729049f67a
Dipak@1432 <chavandipakr17@gmail.com>
2022-01-25 21:44:31 +0530

    Random number

--- SnakeLadder/SnakeLadder/Player.cs
+++ SnakeLadder/SnakeLadder/Player.cs
@@ -8,12 +8,13 @@ namespace SnakeLadder
 {
     internal class Player
     {
-            public int Start = 0;
-            public void GamePlayed()
-            {
-                Console.WriteLine("Game start now");
-
-                Console.WriteLine("Starting point is " + Start); 
-            }
+        public int Start = 0;
+        public int RolledDice;
+        public void GamePlayed()
+        {
+            Random Random = new Random();
+            RolledDice = Random.Next(1, 7);
+            Console.WriteLine("player Roll  Dice number is:" + " " + RolledDice);
         }
+    }
 }
